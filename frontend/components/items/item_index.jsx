c86c6c035f2beec7dce312a84385f7036745d488
Sheava <sheavad@gmail.com>
2020-06-30 16:22:34 -0700

    added styling to index items

--- frontend/components/items/item_index.jsx
+++ frontend/components/items/item_index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import ItemIndexItem from './item_index_item';
-import ItemIndexContainer from './item_index_container';
 
 class ItemIndex extends React.Component {
     constructor(props) {
@@ -16,7 +14,6 @@ class ItemIndex extends React.Component {
         const { items } = this.props;
         return (
             <div className='item-index'>
-                {/* <Route exact path="/items" component={ItemIndexContainer} /> */}
                 <ul className='item-listing'>
                     {items.map(item => <ItemIndexItem key={item.id} item={item}/>)}
                 </ul>
--- frontend/components/items/item_index_item.jsx
+++ frontend/components/items/item_index_item.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 
 const ItemIndexItem = ({ item }) => (
     <li className='item-index-item'>
-        <Link to={`/items/${item.id}`}>
+        <Link className='item-body' to={`/items/${item.id}`}>
             <img className='index-item-photo' src={item.photoUrl}></img>
             <span className='index-item-title'>{item.title}</span>
-            <span className='index-item-price'>{item.price}</span>
+            <span className='index-item-artist'><span className='by'>by </span> {item.artist}</span>
+            <span className='index-item-price'><span className='dollar-sign'>$</span>{item.price}</span>
+            <span className='free-ship-cap'>FREE Shipping by Artazon</span>
         </Link>
     </li>
 );
