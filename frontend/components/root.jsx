df66e505e62ccc90fb0c75de36990871222111c8
Sheava <sheavad@gmail.com>
2020-07-01 18:21:50 -0700

    fixed scroll

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './scroll_to_top';
 import App from './App';
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
+            <ScrollToTop />
             <App />
         </HashRouter>
     </Provider>
