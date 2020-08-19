102479dad88a5214f7506a5028b2a0dc4dee7ed4
Sheava <sheavad@gmail.com>
2020-08-19 16:16:47 -0700

    fix redirect to home on logout

--- frontend/components/user_home/user_home.jsx
+++ frontend/components/user_home/user_home.jsx
@@ -3,48 +3,63 @@ import { Link, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 import SearchBarContainer from '../search/search_bar_container';
 
-const UserHome = props => {
-    
-    const loggedInGreeting = () => (
-        <div className='page'>
-        <Redirect to='/items/'/>
-            <div className='navbar'>
-                {/* <Link to='/'> */}
-                <Link to='/items/' replace>
-                    <div className='nav-logo'>
-                        <div id='artazon-logo' />
-                    </div>
-                </Link>
-                <div className='left-nav-section'>
-                    <div className='nav-searchbar'>
-                        <SearchBarContainer/>
-                    </div>
-                    <div className='account-dropdown-container'>
-                        <div className='account-dropdown'>
-                            <button className='account-container'>
-                                <span className='nav-line-1' id='hello-user'>Hello, {props.currentUser.name.split(" ")[0]}</span>
-                                <br></br>
-                                <span className='nav-line-2'>Account & Lists ▾</span>
-                            </button>
-                            <div className='account-dropdown-content'>
-                                <button className='alt-link' id='sign-out-dropdown' type='submit' onClick={props.logout}>Sign Out</button>
+class UserHome extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.loggedInGreeting = this.loggedInGreeting.bind(this);
+        this.loggedOutGreeting = this.loggedOutGreeting.bind(this);
+        this.logoutToHome = this.logoutToHome.bind(this);
+    }
+
+    logoutToHome() {
+        this.props.logout();
+        this.props.history.push('/items');
+    }
+
+    loggedInGreeting() {
+        return (
+            <div className='page'>
+            <Redirect to='/items/'/>
+                <div className='navbar'>
+                    {/* <Link to='/'> */}
+                    <Link to='/items/' replace>
+                        <div className='nav-logo'>
+                            <div id='artazon-logo' />
+                        </div>
+                    </Link>
+                    <div className='left-nav-section'>
+                        <div className='nav-searchbar'>
+                            <SearchBarContainer/>
+                        </div>
+                        <div className='account-dropdown-container'>
+                            <div className='account-dropdown'>
+                                <button className='account-container'>
+                                    <span className='nav-line-1' id='hello-user'>Hello, {this.props.currentUser.name.split(" ")[0]}</span>
+                                    <br></br>
+                                    <span className='nav-line-2'>Account & Lists ▾</span>
+                                </button>
+                                <div className='account-dropdown-content'>
+                                    <button className='alt-link' id='sign-out-dropdown' type='submit' onClick={this.logoutToHome}>Sign Out</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='nav-cart'>
-                        <Link to='/cart_items'>
-                            <button className='nav-cart-button'><i className="fas fa-shopping-cart" id='cart'></i> Cart</button>
-                        </Link>
+                        <div className='nav-cart'>
+                            <Link to='/cart_items'>
+                                <button className='nav-cart-button'><i className="fas fa-shopping-cart" id='cart'></i> Cart</button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
+                <div className='sub-navbar'>
+                    <p> </p>
+                </div>
             </div>
-            <div className='sub-navbar'>
-                <p> </p>
-            </div>
-        </div>
-    );
+        )
+    };
 
-    const loggedOutGreeting = () => (
+    loggedOutGreeting() {
+        return (
         <div className='page'>
         <Redirect to='/items/' />
             <div className='navbar'>
@@ -86,9 +101,13 @@ const UserHome = props => {
                 <p> </p>
             </div>
         </div>
-    );
+        )
+    };
 
-    return props.currentUser ? loggedInGreeting() : loggedOutGreeting();
+    render () {
+
+        return this.props.currentUser ? this.loggedInGreeting() : this.loggedOutGreeting();
+    }
 };
 
 export default withRouter(UserHome);
--- frontend/components/user_home/user_home_container.jsx
+++ frontend/components/user_home/user_home_container.jsx
@@ -2,12 +2,16 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import UserHome from './user_home';
 
-const mapStateToProps = state => ({
-    currentUser: state.entities.users[state.session.id],
-});
+const mapStateToProps = state => {
+    return {
+        currentUser: state.entities.users[state.session.id],
+    }
+};
 
-const mapDispatchToProps = dispatch => ({
-    logout: () => dispatch(logout()),
-});
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(logout())
+    }
+};
  
 export default connect(mapStateToProps, mapDispatchToProps)(UserHome);
