52d5ab7ffffcd9375fdc194d69c3795dc7c4cefb
Sheava <sheavad@gmail.com>
2020-08-20 16:55:00 -0700

    fix price css

--- frontend/components/cart/cart_items.jsx
+++ frontend/components/cart/cart_items.jsx
@@ -49,37 +49,41 @@ class CartItems extends React.Component {
                     <ul className='cart_items'>
                         { cartItemsArr.map( cartItemId => (
                             <li className='cart-index-item' key={cartItemId}>
-                                <div className='cart-photo-cont'>
-                                    <img className='cart-index-item-photo' src={this.props.userCartItems[cartItemId].photoUrl}></img>
-                                </div>
-                                <div className='mid-side-specific'>
-                                    <Link className='item-body' to={`/items/${this.props.userCartItems[cartItemId].id}`}>
-                                        <span className='cart-item-title'>{this.props.userCartItems[cartItemId].title}</span>
-                                    </Link>
-                                    <span className='cart-item-artist'><span className='by'>by </span> {this.props.userCartItems[cartItemId].artist}</span>
-                                    <span className='in-stock-cart'>In Stock</span>
-                                    <span className='cart-free-ship-cap'>FREE Shipping & FREE Returns</span>
-                                    <label className='gift'>
-                                        <input type='checkbox' className='yes-gift'/><span className='gift-text'>This is a gift</span>
-                                    </label>
-                                    <div className='qty-delete-row'>
-                                        {/* <select className='quantity-cart' value='Qty:'>
-                                            <option value='1'>Qty: 1</option>
-                                            <option value='2'>2</option>
-                                            <option value='3'>3</option>
-                                            <option value='4'>4</option>
-                                            <option value='5'>5</option>
-                                            <option value='6'>6</option>
-                                            <option value='7'>7</option>
-                                            <option value='8'>8</option>
-                                            <option value='9'>9</option>
-                                            <option value='10'>10</option>
-                                        </select> */}
-                                        <button className='delete-cart-item' onClick={() => this.props.deleteCartItem(cartItemId)}>Delete</button>
+                                <div className='main-cont'>
+                                    <div className='cart-photo-cont'>
+                                        <img className='cart-index-item-photo' src={this.props.userCartItems[cartItemId].photoUrl}></img>
+                                    </div>
+                                    <div className='mid-side-specific'>
+                                        <Link className='item-body' to={`/items/${this.props.userCartItems[cartItemId].id}`}>
+                                            <span className='cart-item-title'>{this.props.userCartItems[cartItemId].title}</span>
+                                        </Link>
+                                        <span className='cart-item-artist'><span className='by'>by </span> {this.props.userCartItems[cartItemId].artist}</span>
+                                        <span className='in-stock-cart'>In Stock</span>
+                                        <span className='cart-free-ship-cap'>FREE Shipping & FREE Returns</span>
+                                        <label className='gift'>
+                                            <input type='checkbox' className='yes-gift'/><span className='gift-text'>This is a gift</span>
+                                        </label>
+                                        <div className='qty-delete-row'>
+                                            {/* <select className='quantity-cart' value='Qty:'>
+                                                <option value='1'>Qty: 1</option>
+                                                <option value='2'>2</option>
+                                                <option value='3'>3</option>
+                                                <option value='4'>4</option>
+                                                <option value='5'>5</option>
+                                                <option value='6'>6</option>
+                                                <option value='7'>7</option>
+                                                <option value='8'>8</option>
+                                                <option value='9'>9</option>
+                                                <option value='10'>10</option>
+                                            </select> */}
+                                            <button className='delete-cart-item' onClick={() => this.props.deleteCartItem(cartItemId)}>Delete</button>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className='price-shennanigans'>
-                                    <span className='cart-dollar-sign'>$</span><span className='cart-item-price'>{(Math.round(this.props.userCartItems[cartItemId].price * 100) / 100).toFixed(2)}</span>
+                                <div className='price-cont'>
+                                    <div className='price-shennanigans'>
+                                        <span className='cart-dollar-sign'>$</span><div className='cart-item-price'>{(Math.round(this.props.userCartItems[cartItemId].price * 100) / 100).toFixed(2)}</div>
+                                    </div>
                                 </div>
                             </li>
                         ))}
