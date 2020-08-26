91bb2f1218ffc291ba29ca1cf843f4b33ee8e774
Sheava <sheavad@gmail.com>
2020-08-26 14:25:42 -0700

    remove splash folder, no usage

--- frontend/components/splash/logged_in_splash.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-const LoggedInSplash = props => {
-    <Redirect to='/items/'/>
-}
-
-export default LoggedInSplash;
--- frontend/components/splash/logged_out_splash.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-const LoggedOutSplash = props => {
-    <Redirect to='/items/' />
-}
-
-export default LoggedOutSplash;
