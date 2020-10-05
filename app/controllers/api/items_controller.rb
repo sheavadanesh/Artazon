738da1c87bd343515eff6b90cbd08755ab012b04
Sheava <sheavad@gmail.com>
2020-10-05 13:26:35 -0700

    sdfsflkjs

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,13 +2,11 @@ class Api::ItemsController < ApplicationController
 
     def index 
         @items = keyword ? Item.search(keyword) : Item.all
-        # .all is a built-in Rails method that serves up all records of the object.
         render :index
     end
 
     def show 
         @item = Item.find(params[:id])
-        # .find is also a built-in Rails method
         render :show
     end
 
